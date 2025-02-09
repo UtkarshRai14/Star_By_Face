58ad8ce825fce02d52068316af4c62586a392dc5
Utkarsh Rai <145283694+UtkarshRai14@users.noreply.github.com>
2025-02-09 16:58:32 +0530

    app.py

--- app.py
+++ app.py
@@ -96,4 +96,5 @@ def result_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
